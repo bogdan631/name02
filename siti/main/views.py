bf5bf1c2b2b91b3086538f6cb9bf9910be8d1706
bogdan631 <b.ins@bk.ru>
2024-07-23 21:04:24 +0300

    test

--- siti/main/views.py
+++ siti/main/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Table
 # Create your views here.
 def home (request):
-    return render(request,'main/home.html')
+    table =Table.objects.all()
+    return render(request,'main/home.html',{'table':table})
